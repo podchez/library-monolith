df373507c31cf5c7f863e518c83fd17b2f5da24d
podchez <maxpodchez@gmail.com>
2022-05-06 17:35:38 +0300

    Add some methods like findByUniqueField

--- src/main/java/com/podchez/librarymonolith/repository/AccountRepository.java
+++ src/main/java/com/podchez/librarymonolith/repository/AccountRepository.java
@@ -3,5 +3,8 @@ package com.podchez.librarymonolith.repository;
 import com.podchez.librarymonolith.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AccountRepository extends JpaRepository<Account, Long> {
+    Optional<Account> findByEmail(String email);
 }
--- src/main/java/com/podchez/librarymonolith/repository/GenreRepository.java
+++ src/main/java/com/podchez/librarymonolith/repository/GenreRepository.java
@@ -3,5 +3,8 @@ package com.podchez.librarymonolith.repository;
 import com.podchez.librarymonolith.entity.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface GenreRepository extends JpaRepository<Genre, Integer> {
+    Optional<Genre> findByName(String name);
 }
