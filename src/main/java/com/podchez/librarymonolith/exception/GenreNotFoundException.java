cc0518abda7ad8ce20a8b9f805af63ffb0c95cd0
podchez <maxpodchez@gmail.com>
2022-05-07 22:38:50 +0300

    Update EntityNotFound exceptions

--- src/main/java/com/podchez/librarymonolith/exception/GenreNotFoundException.java
+++ src/main/java/com/podchez/librarymonolith/exception/GenreNotFoundException.java
@@ -4,4 +4,8 @@ public class GenreNotFoundException extends RuntimeException {
     public GenreNotFoundException(Integer id) {
         super("There is no genre with ID = " + id + " in the database.");
     }
+
+    public GenreNotFoundException(String name) {
+        super("There is no genre with name '" + name + "' in the database.");
+    }
 }
