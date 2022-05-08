fc4869598f02266a8c1c46b740b877ceef64a715
podchez <maxpodchez@gmail.com>
2022-05-08 15:12:53 +0300

    Add REST Controllers with CRUD methods

--- src/main/java/com/podchez/librarymonolith/controller/AuthorController.java
+++ src/main/java/com/podchez/librarymonolith/controller/AuthorController.java
@@ -1,9 +1,58 @@
 package com.podchez.librarymonolith.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.podchez.librarymonolith.dto.AuthorRequestDto;
+import com.podchez.librarymonolith.dto.AuthorResponseDto;
+import com.podchez.librarymonolith.service.AuthorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/authors")
 public class AuthorController {
+
+    private final AuthorService authorService;
+
+    @Autowired
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK) // 200
+    public List<AuthorResponseDto> findAll() {
+        return authorService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK) // 200
+    public AuthorResponseDto findById(@PathVariable Long id) {
+        return authorService.findById(id);
+    }
+
+    @GetMapping("/fullName/{fullName}")
+    @ResponseStatus(HttpStatus.OK) // 200
+    public AuthorResponseDto findByFullName(@PathVariable String fullName) {
+        return authorService.findByFullName(fullName);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED) // 201
+    public AuthorResponseDto save(@RequestBody AuthorRequestDto authorReqDto) {
+        return authorService.save(authorReqDto);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK) // 200
+    public AuthorResponseDto update(@PathVariable Long id, @RequestBody AuthorRequestDto authorReqDto) {
+        return authorService.update(id, authorReqDto);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT) // 204
+    public void deleteById(@PathVariable Long id) {
+        authorService.deleteById(id);
+    }
 }
--- src/main/java/com/podchez/librarymonolith/controller/BookController.java
+++ src/main/java/com/podchez/librarymonolith/controller/BookController.java
@@ -1,9 +1,58 @@
 package com.podchez.librarymonolith.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.podchez.librarymonolith.dto.BookRequestDto;
+import com.podchez.librarymonolith.dto.BookResponseDto;
+import com.podchez.librarymonolith.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/books")
 public class BookController {
+
+    private final BookService bookService;
+
+    @Autowired
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK) // 200
+    public List<BookResponseDto> findAll() {
+        return bookService.findAll();
+    }
+
+    @GetMapping(params = {"title"})
+    @ResponseStatus(HttpStatus.OK) // 200
+    public List<BookResponseDto> findAllByTitle(@RequestParam String title) {
+        return bookService.findAllByTitle(title);
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK) // 200
+    public BookResponseDto findById(@PathVariable Long id) {
+        return bookService.findById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED) // 201
+    public BookResponseDto save(@RequestBody BookRequestDto bookReqDto) {
+        return bookService.save(bookReqDto);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK) // 200
+    public BookResponseDto update(@PathVariable Long id, @RequestBody BookRequestDto bookReqDto) {
+        return bookService.update(id, bookReqDto);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT) // 204
+    public void deleteById(@PathVariable Long id) {
+        bookService.deleteById(id);
+    }
 }
