5f0524a7ae7737ad2129bb28b0842f255014398b
podchez <maxpodchez@gmail.com>
2022-07-10 00:02:39 +0300

    Override equals() and hashCode()

--- src/main/java/com/podchez/librarymonolith/model/Book.java
+++ src/main/java/com/podchez/librarymonolith/model/Book.java
@@ -3,7 +3,6 @@ package com.podchez.librarymonolith.model;
 import lombok.*;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.Objects;
