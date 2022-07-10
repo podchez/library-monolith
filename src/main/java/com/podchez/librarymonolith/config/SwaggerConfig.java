ed79106f6430beb9c6d690f1526676d91b20f7b5
podchez <maxpodchez@gmail.com>
2022-07-10 15:45:31 +0300

    Add and configure Swagger for API Documentation

---
+++ src/main/java/com/podchez/librarymonolith/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.podchez.librarymonolith.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .paths(PathSelectors.ant("/api/**"))
+                .apis(RequestHandlerSelectors.basePackage("com.podchez.librarymonolith"))
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Library REST API")
+                .description("books, authors, accounts, roles")
+                .version("1.0")
+                .contact(new Contact("My GitHub profile", "https://github.com/podchez", null))
+                .build();
+    }
+}
